68e02f06768f34c6576f061abce6e9f124d1d491
Karan Jayachandra <karan.jayachandra@nxp.com>
2023-03-16 14:01:29 +0100

    Cleaned up the code and added classes

--- loan_visualizer.py
+++ loan_visualizer.py
@@ -1,48 +1,99 @@
-from math import floor
-from numpy import arange
+from dataclasses import dataclass
 import matplotlib.pyplot as plot
 
-BAR_WIDTH = 0.3
-EXCHANGE_RATE = 85
-MAX_SAVINGS = 1800
-interest_rate = 13.5
-monthly_interest_rate = interest_rate / 12
-init_balance = 2300089
-init_balance = 2083190
-emi_inr_min = 42780
-emi_eur_min = round(emi_inr_min / EXCHANGE_RATE)
-emi_eur = [emi_eur_min, 600, 800, 1000, 1200, 1400, 1600, 1800]
+def currency_round(input):
+    return round(input, 2)
 
-total_interest_paid  = []
-total_savings_yearly = []
+@dataclass
+class Loan():
+    balance: float = 2083190
+    yearly_interest_rate: float = 13.5
+    monthly_interest_points: float = yearly_interest_rate / 1200
+    minimum_emi: float = 42780
+    currency: str = 'INR'
 
+@dataclass
+class Budget():
+    rent: float = 700
+    insurance: float = 150
+    travel: float = 350
+    needs: float = 300
+    wants: float = 300
 
-for emi_candidate in emi_eur:
-    balance = [init_balance]
-    interest = []
-    current_balance = init_balance
-    emi_inr = emi_candidate * EXCHANGE_RATE
-    while current_balance > 0:
-        current_interest = current_balance * monthly_interest_rate / 100
-        interest.append(current_interest)
-        current_balance = current_balance + current_interest
-        current_balance = current_balance - emi_inr
-        balance.append(current_balance)
-    print(f"EMI Value: {round(emi_inr, 2)}")
-    print(f"Payment Period: {floor(len(balance) / 12)} years, {len(balance) % 12} months")
-    print(f"Total Interest Paid: {round(sum(interest), 2)}")
-    total_interest_paid.append(round(sum(interest) / 1e5, 2))
-    total_savings_yearly.append(((MAX_SAVINGS - emi_candidate) * 85 * 12) / 1e5)
-print(emi_eur)
-print(total_interest_paid)
-print(total_savings_yearly)
+    def get_total_budget(self):
+        total = 0
+        for field in self.__dataclass_fields__:
+            total += getattr(self, field)
+        return total
 
-br1 = arange(len(emi_eur))
-br2 = [x + BAR_WIDTH for x in br1]
-plot.bar(br1, total_interest_paid, width=BAR_WIDTH, label='Total Interest Paid')
-plot.bar(br2, total_savings_yearly, width=BAR_WIDTH, label='Yearly Savings')
-plot.xticks([r + BAR_WIDTH/2 for r in range(len(emi_eur))], emi_eur)
-plot.xlabel('EMI (\u20AC)')
-plot.ylabel('Lakhs (\u20B9)')
-plot.legend()
-plot.show()
+class Income():
+    total_income: float = 3500
+    budget = Budget()
+    total_expenses: float = budget.get_total_budget()
+    total_savings: float = total_income - total_expenses
+    currency: str = 'EUR'
+
+class LoanPayment():
+    loan = Loan()
+    income = Income()
+    EXCHANGE_RATE = 85
+    BAR_WIDTH = 0.3
+    strategies_established: bool = False
+
+    def establish_strategies(self, emi_candidates, candidate_currency):
+        if self.loan.currency == 'INR':
+            emi_min = round(self.loan.minimum_emi / self.EXCHANGE_RATE, 2)
+        if candidate_currency == 'INR':
+            emi_candidates == [round(x / self.EXCHANGE_RATE, 2) for x in emi_candidates]
+        emi_candidates.insert(0, emi_min)
+        self.emi_candidates = emi_candidates
+        self.strategies_established = True
+
+    def evaluate_strategies(self):
+        self.interest_paid  = []
+        self.yearly_savings = []
+        self.payment_term   = []
+
+        for emi_candidate in self.emi_candidates:
+            interest = []
+            current_emi = emi_candidate * self.EXCHANGE_RATE
+            monthly_saving = self.income.total_savings - emi_candidate
+            yearly_saving = monthly_saving * 12 * self.EXCHANGE_RATE
+            current_balance = self.loan.balance
+            while current_balance > 0:
+                current_interest = current_balance * self.loan.monthly_interest_points
+                current_balance = current_balance + current_interest
+                current_balance = current_balance - current_emi
+                interest.append(current_interest)
+            self.payment_term.append(len(interest))
+            self.interest_paid.append(currency_round(sum(interest) / 1e5))
+            self.yearly_savings.append(currency_round(yearly_saving / 1e5))
+
+    def visualize_output(self):
+        _, ax1 = plot.subplots()
+        ax1.set_xlabel('EMI (\u20AC)')
+        ax1.set_ylabel('Lakh (\u20B9)')
+        br1 = [x for x in range(len(self.emi_candidates))]
+        br2 = [x + self.BAR_WIDTH for x in br1]
+        ax1.bar(br1, self.interest_paid, width=self.BAR_WIDTH, label='Total Interest Paid')
+        ax1.bar(br2, self.yearly_savings, width=self.BAR_WIDTH, label='Yearly Savings')
+        tick_positions = [r + self.BAR_WIDTH/2 for r in range(len(self.emi_candidates))]
+        ax1.set_xticks(tick_positions, self.emi_candidates)
+        plot.legend(loc='upper right')
+
+        ax2 = ax1.twinx()
+        ax2.plot(br1, self.payment_term, 'k', linewidth=2, label='Payment Term')
+        ax2.set_ylabel('Months')
+        plot.legend(loc='center right')
+        
+        plot.show()
+
+def main():
+    emi_eur = [600, 800, 1000, 1200, 1400, 1600]
+    lp = LoanPayment()
+    lp.establish_strategies(emi_eur, 'EUR')
+    lp.evaluate_strategies()
+    lp.visualize_output()
+
+if __name__ == '__main__':
+    main()
